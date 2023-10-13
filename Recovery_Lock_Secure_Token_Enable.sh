3fd9c1e72aa5f6c54cd0b887bc75a11b81e2df17
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2023-10-13 12:18:32 -0700

    Create Recovery_Lock_Secure_Token_Enable.sh

---
+++ Recovery_Lock_Secure_Token_Enable.sh
@@ -0,0 +1,3 @@
+sudo sysadminctl -adminUser SD\ Tech -adminPassword hellopawsswd -secureTokenOn Admin -password \$D123456!
+
+sudo sysadminctl -adminUser SD\ Tech -adminPassword hellopaswws -secureTokenOn F1 -password \$D123456!
