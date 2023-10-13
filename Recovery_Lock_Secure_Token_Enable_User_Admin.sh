e7aa88d1a1a36c59d3411dc99472453a3da2c3f1
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2023-10-13 11:33:48 -0700

    Update Recovery_Lock_Secure_Token_Enable_User_Admin.sh

--- Recovery_Lock_Secure_Token_Enable_User_Admin.sh
+++ Recovery_Lock_Secure_Token_Enable_User_Admin.sh
@@ -1,3 +1,1 @@
-sudo sysadminctl -adminUser SD\ Tech -adminPassword safetyzone -secureTokenOn Admin -password \$D123456!
 
-sudo sysadminctl -adminUser SD\ Tech -adminPassword safetyzone -secureTokenOn F1 -password \$D123456!
